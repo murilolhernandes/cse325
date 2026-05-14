4b2c428c9158af45d3527a193c6d091bf9a4ba74
murilolhernandes <murilo.luiz.hernandes@gmail.com>
2026-05-13 18:45:22 -0600

    Finished learning activity

--- ContosoPizza/obj/Debug/net8.0/ContosoPizza.AssemblyInfo.cs
+++ ContosoPizza/obj/Debug/net8.0/ContosoPizza.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+227f3167f12bc5ffab458e4f79b2f9dc872cff09")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4ceab1a108e2cf67f3f733d298b495fded02661")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
