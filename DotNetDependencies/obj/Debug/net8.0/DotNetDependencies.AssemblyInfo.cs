105b0e23532ad8bc8ee6927d5db037292e939afb
murilolhernandes <murilo.luiz.hernandes@gmail.com>
2026-05-13 18:59:14 -0600

    Try 1

--- DotNetDependencies/obj/Debug/net8.0/DotNetDependencies.AssemblyInfo.cs
+++ DotNetDependencies/obj/Debug/net8.0/DotNetDependencies.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotNetDependencies")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b2c428c9158af45d3527a193c6d091bf9a4ba74")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1354e0280f10751a9454493379f9ec32be84ed56")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotNetDependencies")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DotNetDependencies")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
