988dc444894e59f97a1c95b864f54361914f7623
murilolhernandes <murilo.luiz.hernandes@gmail.com>
2026-05-14 22:02:50 -0600

    Week 2 Movie Assignment Done

--- DotNetDependencies/obj/Debug/net8.0/DotNetDependencies.AssemblyInfo.cs
+++ DotNetDependencies/obj/Debug/net8.0/DotNetDependencies.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotNetDependencies")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+105b0e23532ad8bc8ee6927d5db037292e939afb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48589c996b9ee071073da2fdfde5b5d88a570b50")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotNetDependencies")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DotNetDependencies")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
