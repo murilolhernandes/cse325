c205b61499ca700b16b6475946603e4efe94610b
murilolhernandes <murilo.luiz.hernandes@gmail.com>
2026-05-29 18:27:31 -0600

    Finished the Tutorial

--- DotNetDebugging/obj/Debug/net10.0/DotNetDebugging.AssemblyInfo.cs
+++ DotNetDebugging/obj/Debug/net10.0/DotNetDebugging.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotNetDebugging")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1dc7458509185378a5dcf4e3d159d6307127bbb0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9fcd02654d451348d096cf0963402907fe6f9fc6")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotNetDebugging")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DotNetDebugging")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- FirstClassLibrary/obj/Debug/net8.0/FirstClassLibrary.AssemblyInfo.cs
+++ FirstClassLibrary/obj/Debug/net8.0/FirstClassLibrary.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("This is a Razor component library with a cool modal window component.")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("0.1.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("0.1.0+1dc7458509185378a5dcf4e3d159d6307127bbb0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("0.1.0+9fcd02654d451348d096cf0963402907fe6f9fc6")]
 [assembly: System.Reflection.AssemblyProductAttribute("FirstClassLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FirstClassLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("0.1.0.0")]
